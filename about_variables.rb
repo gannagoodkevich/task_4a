64143fb5f23c61eaab483461666ae2a9641bfa93
gannagoodkevich <gannagoodkevich@gmail.com>
2019-11-13 16:21:55 +0300

    fix spacing

--- about_variables.rb
+++ about_variables.rb
@@ -1,36 +1,36 @@
 class AboutVariables
-foo = 'local foo'
- 	@@foo = 'was '  + foo + ", became class foo"
+  foo = 'local foo'
+  @@foo = 'was '  + foo + ", became class foo"
 
-def initialize
+  def initialize
 		@foo = 'instance foo'
-end
+  end
 
-  	def show_local_foo
-		    puts foo #local_foo
+	def show_local_foo
+	   puts foo #local_foo
+	end
+
+	def show_class_foo
+  	puts @@foo
+	end
+
+	def show_instance_foo
+ 		puts @foo
+	end
+
+	class << self
+ 		def show_local_foo
+			puts foo #local_foo
+	  end
+
+	  def show_class_foo
+			puts @@foo
   	end
 
-  	def show_class_foo
-    		puts @@foo
-  	end
-
-  	def show_instance_foo
-   		puts @foo
-  	end
-
-  	class << self
-     		def show_local_foo
-    			puts foo #local_foo
-  		  end
-
-  		  def show_class_foo
-    			puts @@foo
-  	  	end
-
-  		  def show_instance_foo
-    		 	puts @foo
-  		  end
-    end
+	  def show_instance_foo
+		 	puts @foo
+	  end
+  end
 end
 
 AboutVariables.show_class_foo
